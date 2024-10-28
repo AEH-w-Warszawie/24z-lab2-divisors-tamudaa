a1a7be349c6d238c8b409bec41bd9b9c359359d6
tamudaa <mahlata9236_aehit@students.vizja.pl>
2024-10-29 00:18:32 +0100

    Updated code

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -7,7 +7,12 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
 		if (number % i == 0)
+		{
 			result.push_back(i);
+			if (i != number / i)
+			 result.push_back(number / i);
+		}
+			
 	}
 	return result;
 }
